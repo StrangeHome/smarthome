c71154eab4083ae7505b43b0ac6535b7a1149523
haydenmcp <hayden.mcparlane@gmail.com>
2019-07-06 16:21:28 -0500

    introducing testing dependencies first to prep for TDD

---
+++ test/src/main.cpp
@@ -0,0 +1,9 @@
+#pragma once
+
+#include "includes.hpp"
+
+int main(int argc, char** argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    ::testing::InitGoogleMock(&argc, argv);
+    return RUN_ALL_TESTS();
+}
