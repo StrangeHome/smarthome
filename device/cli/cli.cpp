689c1e23d79053ef7d89fb1a2d81db3f28bcd40c
haydenmcp <hayden.mcparlane@gmail.com>
2020-11-04 16:31:10 -0500

    Ensured test CLI entry operates as expected

--- device/cli/cli.cpp
+++ device/cli/cli.cpp
@@ -1,11 +1,18 @@
 
+#include <CLI/CLI.hpp>
 #include <CLI/App.hpp>
 
-int main(const int argc, char** argv) {
+int main(int argc, char** argv) {
 
     CLI::App cli{"Command-line Interface into arbitrary smart devices."};
 
+
+    bool test = false;
+    cli.add_flag("-t", test, "A test flag.");
+
     CLI11_PARSE(cli, argc, argv);
+
+    std::cout << "Flag: " << std::to_string(test);
 
     return 0;
 }
