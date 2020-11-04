f0e1d8945ad19154e9cfe4840c524771f464a57e
haydenmcp <hayden.mcparlane@gmail.com>
2020-11-04 18:38:23 -0500

    Began implementation of colored application logger. Still have to fix an issue and verify the results.

--- device/cli/cli.cpp
+++ device/cli/cli.cpp
@@ -1,15 +1,22 @@
 
+#include <smarthome/logger/logger.hpp>
+
 #include <CLI/CLI.hpp>
 #include <CLI/App.hpp>
+#include <string>
 
 int main(int argc, char** argv) {
 
+    auto logger = smarthome::Logger();
     CLI::App cli{"Command-line Interface into arbitrary smart devices."};
 
     bool start = false;
     cli.add_option("start", start, "Start the device.");
 
     CLI11_PARSE(cli, argc, argv);
+
+    if (start)
+        logger.info("Starting device...");
 
     return 0;
 }
--- logger/include/smarthome/logger/logger.hpp
+++ logger/include/smarthome/logger/logger.hpp
@@ -1,2 +1,49 @@
 
-// TODO: Define logger class to encapsulate logging for app
+#include <string>
+#include <spdlog/spdlog.h>
+
+namespace smarthome {
+
+    /**
+     * Pure abstract base application logger.
+    */
+    class Logger {
+        public:
+            /**
+             * Log the specified debug message.
+             *
+             * @param msg Message to log.
+             */
+            void debug(const std::string msg);
+
+            /**
+             * Log the specified informational message.
+             *
+             * @param msg Message to log.
+             */
+            void info(const std::string msg);
+
+            /**
+             * Log the specified warning message.
+             *
+             * @param msg Message to log.
+             */
+            void warn(const std::string msg);
+
+            /**
+             * Log the specified error message.
+             *
+             * @param msg Message to log.
+             */
+            void error(const std::string msg);
+
+            /**
+             * Log the specified critical message.
+             *
+             * @param msg Message to log.
+             */
+            void critical(const std::string msg);
+
+    };
+
+} // namespace smarthome
