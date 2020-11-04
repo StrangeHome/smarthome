cad730e90536405a92a4932651af37ecbd00434b
haydenmcp <hayden.mcparlane@gmail.com>
2020-11-04 16:22:54 -0500

    Began shelling out smart device CLI for testing with BeagelBone Black

--- device/cli/cli.cpp
+++ device/cli/cli.cpp
@@ -1,6 +1,11 @@
 
+#include <CLI/App.hpp>
 
-int main() {
+int main(const int argc, char** argv) {
+
+    CLI::App cli{"Command-line Interface into arbitrary smart devices."};
+
+    CLI11_PARSE(cli, argc, argv);
 
     return 0;
 }
