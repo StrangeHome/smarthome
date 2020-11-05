0765c8adc1b9ed8127b43ddfe1f6a66679d0fce8
haydenmcp <hayden.mcparlane@gmail.com>
2020-11-04 19:13:17 -0500

    Added TODOs for next steps

--- device/cli/cli.cpp
+++ device/cli/cli.cpp
@@ -5,10 +5,16 @@
 #include <CLI/App.hpp>
 #include <string>
 
+// TODO List
+//  1. Create automation scripts in Python (platform agnostic) to configure BeagelBone Black
+//  for device run (i.e, ansible may be used to deploy to devices)
+//  2. Implement dependency injection subsystem for mockability of software components. This
+//  requires pure abstract bases.
+
 int main(int argc, char** argv) {
 
     auto logger = smarthome::Logger();
-    CLI::App cli{"Command-line Interface into arbitrary smart devices."};
+    CLI::App cli{"Command-line Interface into smarthome devices."};
 
     bool start = false;
     cli.add_flag("-s,--start", start, "Start the device.");
