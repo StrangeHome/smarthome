4def5db0044136ca6c81e62c53d42182ea87a561
haydenmcp <hayden.mcparlane@gmail.com>
2020-11-06 09:41:33 -0500

    Stubbed out tests to start TDD of service lib

---
+++ service/test/src/service_factory.integration.tests.cpp
@@ -0,0 +1,9 @@
+#include <gmock/gmock.h>
+
+TEST(ServiceFactory, ShouldAllowUserToRegisterConcreteImplementations) {
+    // TODO
+}
+
+TEST(ServiceFactory, ShouldAllowUserToFetchAConcreteImplementationUsingTheAbstractBaseClass) {
+    // TODO
+}
