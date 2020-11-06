bebf485e67338879b6b1c7e741d66b6e12fd884c
haydenmcp <hayden.mcparlane@gmail.com>
2020-11-06 09:32:33 -0500

    Introduced mock library to start TDD of service lib

--- service/test/src/main.cpp
+++ service/test/src/main.cpp
@@ -1,7 +1,9 @@
-#include "includes.hpp"
+#include <gtest/gtest.h>
+#include <gmock/gmock.h>
+#include <smarthome/mock/all.hpp>
 
 int main(int argc, char** argv) {
-    ::testing::InitGoogleTest(&argc, argv);
+
     ::testing::InitGoogleMock(&argc, argv);
     return RUN_ALL_TESTS();
 }
