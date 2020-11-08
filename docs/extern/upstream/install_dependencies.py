2e71d5ddc6a8c4655ed8fe77804326a3d0e4f97a
haydenmcp <hayden.mcparlane@gmail.com>
2020-11-08 12:13:53 -0500

    Began incorporation of doxygen and generation of documentation from CMake

--- docs/extern/upstream/install_dependencies.py
+++ docs/extern/upstream/install_dependencies.py
@@ -1,16 +1,17 @@
 import subprocess
 
 def install_dependencies():
-    success = False
+    installed = subprocess.run(["which", "doxygen"])
+    if (installed):
+        return;
+
     aptitude = subprocess.run(["which", "apt-get"])
     if (aptitude):
         subprocess.run(["sudo", "apt-get", "update"])
-        success = subprocess.run(["sudo", "apt-get", "-y", "install", "bison", "flex", "doxygen"])
+        subprocess.run(["sudo", "apt-get", "-y", "install", "bison", "flex", "graphviz", "doxygen"])
     else:
         subprocess.run(["sudo", "yum", "update"])
-        success = subprocess.run(["sudo", "yum", "-y", "install", "bison", "flex", "doxygen"])
-
-    return success
+        subprocess.run(["sudo", "yum", "-y", "install", "bison", "flex", "graphviz", "doxygen"])
 
 
 if __name__ == "__main__":
