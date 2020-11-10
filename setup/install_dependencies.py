ff72e9b4f8dd35be52a9fa99317522edb4325933
haydenmcp <hayden.mcparlane@gmail.com>
2020-11-10 18:31:52 -0500

    Creating automated setup config/script.

--- setup/install_dependencies.py
+++ setup/install_dependencies.py
@@ -4,16 +4,17 @@ import sys
 def install_dependencies():
 
     package_manager = "apt-get"
-    aptitude = subprocess.run(["which", "apt-get"])
-    if (not aptitude):
+    try:
+        subprocess.run(["which", "apt-get"]).check_returncode()
+    except:
         package_manager = "yum"
 
     dependencies = sys.argv[1:]
 
-    print("Updating OS package manager.\n", sys.stdout)
+    print("Running: sudo " + package_manager + " update")
     subprocess.run(["sudo", package_manager, "update"])
 
-    print("Installing dependencies: " + str(dependencies) + "\n", sys.stdout)
+    print("Running: sudo " + package_manager + " -y install " + str(dependencies))
     subprocess.run(["sudo", package_manager, "-y", "install", *dependencies])
 
 if __name__ == "__main__":
